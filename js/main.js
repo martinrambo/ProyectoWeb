8973aa68520d90d59540ef34d051e64cfeefa11e
martinrambo <dieg.nunezt@duocuc.cl>
2023-05-07 18:44:46 -0400

    Guardado 1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+
+});
